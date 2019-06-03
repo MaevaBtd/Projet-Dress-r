8bb8bbb352350c939d02b1f9faa6193d943df1ae
MaevaBtd <melizenn@gmail.com>
2019-06-03 22:37:20 +0200

    structure cloth et clothlist ok

--- src/components/Cloth/ClothList.js
+++ src/components/Cloth/ClothList.js
@@ -1,11 +1,49 @@
 // == Import: Yarn
 import React from 'react';
+import { Button, Icon } from 'antd';
+import 'antd/dist/antd.css';
 
 // == Import: local
 import './ClothList.scss';
+import Cloth from '.';
 
 // == Code
-const Cloth = () => ();
+const ClothList = () => (
+  <div id="clothlist">
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+  </div>
+);
 
 // == Export
 export default ClothList;
