5ef687ce67f4d6e4932835cf8f07649a373e4c7c
8kmh <maida.maxime@gmail.com>
2019-06-03 17:45:36 +0200

    header main footer OK

--- src/components/Header /index.js
+++ src/components/Header /index.js
@@ -2,17 +2,20 @@
 import React from 'react';
 
 // == Import : local
-import './index.scss';
+import './header.scss';
 
 // == Composant
 const Header = () => (
-  <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Logo</a>
-      <ul id="nav">
-        <li><a href="#">Connexion</a></li>
-        <li><a href="#">Inscription</a></li>
-      </ul>
+  <nav className=" navbar navbar-expand-lg navbar-dark fixed-top">
+    <a href=""><h1 className="nav-title" href="#">Dress'R</h1></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-light navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav ml-auto">
+        <a className="nav-item nav-link" href="#">Inscription <span className="sr-only">(current)</span></a>
+        <a className="nav-item nav-link" href="#">Connexion</a>
+      </div>
     </div>
   </nav>
 );
