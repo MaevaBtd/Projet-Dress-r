5c9f01bb84bd0c1d8443584e94c7c1ecafa7e81f
8kmh <maida.maxime@gmail.com>
2019-06-03 09:18:17 +0200

    footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,19 @@
 // == Import : npm
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 
 // == Import : local
 import './app.scss';
+import Footer from 'src/components/Footer';
 
+
+library.add(fab);
 // == Composant
 const App = () => (
   <div id="app">
-    App React
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+// Import npm
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// import PropTypes from 'prop-types';
+
+// Import local
+import './footer.scss';
+
+// Component
+const Footer = () => (
+  <div id="footer">
+    <p id="footer-title">- &copy; Copyright - Dress'R 2019 </p>
+    <a href="#"><FontAwesomeIcon size="2x" className="icon" icon={['fab', 'facebook']} /></a>
+    <a href="#"><FontAwesomeIcon size="2x" className="icon" icon={['fab', 'twitter']} /></a>
+    <a href="#"><FontAwesomeIcon size="2x" className="icon" icon={['fab', 'instagram']} /></a>
+  </div>
+);
+
+// Export
+export default Footer;
