2387f6ae8dcf62b2ee9941105755e1d70ebdd743
KevinDumetz <k.dumetz@live.fr>
2019-06-03 09:52:20 +0200

    Creation des entités et des relations

---
+++ src/Entity/Style.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StyleRepository")
+ */
+class Style
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Cloth", mappedBy="styles")
+     */
+    private $cloths;
+
+    public function __construct()
+    {
+        $this->cloths = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Cloth[]
+     */
+    public function getCloths(): Collection
+    {
+        return $this->cloths;
+    }
+
+    public function addCloth(Cloth $cloth): self
+    {
+        if (!$this->cloths->contains($cloth)) {
+            $this->cloths[] = $cloth;
+            $cloth->addStyle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCloth(Cloth $cloth): self
+    {
+        if ($this->cloths->contains($cloth)) {
+            $this->cloths->removeElement($cloth);
+            $cloth->removeStyle($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Type.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TypeRepository")
+ */
+class Type
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Cloth", mappedBy="type")
+     */
+    private $cloths;
+
+    public function __construct()
+    {
+        $this->cloths = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Cloth[]
+     */
+    public function getCloths(): Collection
+    {
+        return $this->cloths;
+    }
+
+    public function addCloth(Cloth $cloth): self
+    {
+        if (!$this->cloths->contains($cloth)) {
+            $this->cloths[] = $cloth;
+            $cloth->setType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCloth(Cloth $cloth): self
+    {
+        if ($this->cloths->contains($cloth)) {
+            $this->cloths->removeElement($cloth);
+            // set the owning side to null (unless already changed)
+            if ($cloth->getType() === $this) {
+                $cloth->setType(null);
+            }
+        }
+
+        return $this;
+    }
+}
