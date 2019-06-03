1505aac94677707ad16af06c651a72c72d5ef669
MaevaBtd <melizenn@gmail.com>
2019-06-03 22:58:15 +0200

    structure outfit et wardrobe ok

--- src/components/Outfit/index.js
+++ src/components/Outfit/index.js
@@ -1,11 +1,38 @@
 // == Import: Yarn
 import React from 'react';
+import { Button, Icon } from 'antd';
+import 'antd/dist/antd.css';
 
 // == Import: local
 import './Outfit.scss';
+import Cloth from '../Cloth';
 
 // == Code
-const Outfit = () => ();
+const Outfit = () => (
+  <div id="clothlist">
+    <h1>Nom de la Tenue</h1>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+    <Cloth />
+    <Button shape="circle" >
+      <Icon type="delete" theme="filled" />
+    </Button>
+  </div>
+);
 
 // == Export
 export default Outfit;
