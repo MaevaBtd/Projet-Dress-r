72e1eeb9d056b038852401d17f5a207415af9e73
MaevaBtd <melizenn@gmail.com>
2019-06-03 09:31:04 +0200

    ajout antd

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -11,6 +11,7 @@ const Accueil = () => (
   <div>
     <Welcome />
     <About />
+    
   </div>
 );
 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // == Import : local
 import './app.scss';
+import Accueil from 'src/components/Accueil';
 import Footer from 'src/components/Footer';
 
 
@@ -13,6 +14,7 @@ library.add(fab);
 // == Composant
 const App = () => (
   <div id="app">
+    <Accueil />
     <Footer />
   </div>
 );
