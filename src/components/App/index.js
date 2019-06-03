3c322ce95c139febb06421122eed51a43fcfdfb1
8kmh <maida.maxime@gmail.com>
2019-06-03 18:33:20 +0200

    front page ok 18h30

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,5 @@
 // == Import : npm
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // == Import : local
 import './app.scss';
@@ -9,12 +7,10 @@ import './app.scss';
 import Accueil from 'src/components/Accueil';
 // import Footer from 'src/components/Footer';
 
-
-
 // == Composant
 const App = () => (
   <div id="app">
-
+    <Accueil />
   </div>
 );
 
