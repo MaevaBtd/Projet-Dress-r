3ffe60ff721917845c44e24029365a957d565461
MaevaBtd <melizenn@gmail.com>
2019-06-04 12:49:10 +0200

    structure random et profil ok

---
+++ src/components/Profil/ChangeMdp.js
@@ -0,0 +1,11 @@
+// == Import: Yarn
+import React from 'react';
+
+// == Import: local
+import './ChangeMdp.scss';
+
+// == Code
+const ChangeMdp = () => ();
+
+// == Export
+export default ChangeMdp;
