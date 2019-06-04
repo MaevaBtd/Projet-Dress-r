6748737b3764b6be575897ed13bfa6ba14305a15
8kmh <maida.maxime@gmail.com>
2019-06-04 23:43:52 +0200

    Commentary App rmb wtd

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,9 +7,13 @@ import './app.scss';
 import Accueil from 'src/components/Accueil';
 import Footer from 'src/components/Footer';
 import Header from '../Header ';
+// btn submit display none mobile
 import FormAddCloth from '../AddCloth/FormAddCloth';
+// à faire
 import AddCloth from '../AddCloth';
+// à refaire
 import Clothlist from '../Cloth/ClothList';
+// à faire
 import Cloth from '../Cloth';
 import Main from '../Main';
 import Changemdp from '../Profil/ChangeMdp';
