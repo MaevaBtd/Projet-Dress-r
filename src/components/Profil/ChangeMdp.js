27342a831d998a2646bf33eb04b56bc922d7d500
MaevaBtd <melizenn@gmail.com>
2019-06-04 13:57:00 +0200

    structure change pwd ok

--- src/components/Profil/ChangeMdp.js
+++ src/components/Profil/ChangeMdp.js
@@ -1,11 +1,42 @@
 // == Import: Yarn
 import React from 'react';
+import { Form, Icon, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
 
 // == Import: local
 import './ChangeMdp.scss';
 
 // == Code
-const ChangeMdp = () => ();
+const ChangeMdp = () => (
+  <Form className="change-pwd-form">
+    <Form.Item>
+      <Input
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="Ancien Mot de passe"
+      />
+    </Form.Item>
+    <Form.Item>
+      <Input
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        type="password"
+        placeholder="Nouveau Mot de passe"
+      />
+    </Form.Item>
+    <Form.Item>
+      <Input
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        type="password"
+        placeholder="Répétez le nouveau Mot de passe"
+      />
+    </Form.Item>
+    <Form.Item>
+      <Button type="primary" htmlType="submit" className="valider">
+        Valider
+      </Button>
+      <a href="">Annuler</a>
+    </Form.Item>
+  </Form>
+);
 
 // == Export
 export default ChangeMdp;
