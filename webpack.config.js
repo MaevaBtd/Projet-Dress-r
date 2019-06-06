8084c71e86e3183add7565fe6f0692686f6552ab
MaevaBtd <melizenn@gmail.com>
2019-06-06 16:32:20 +0200

    maj webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 // Plugins de traitement pour dist/
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
@@ -50,7 +50,7 @@ module.exports = {
     },
     // Minification
     minimizer: [
-      new UglifyJsPlugin({
+      new TerserPlugin({
         cache: true,
         parallel: true,
         sourceMap: false, // passer à true pour JS source maps
