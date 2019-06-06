d0707b4b47f9d56bfad13dfd3bbb50cf66a61a59
MaevaBtd <melizenn@gmail.com>
2019-06-06 09:23:40 +0200

    appel API ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,25 @@
 // == Import : npm
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 // == Import : local
 import './app.scss';
 
 // peaufiner toute les size dès que toute l'inté est faite
-
+import Wardrobe from 'src/containers/Wardrobe';
 import Accueil from 'src/components/Accueil';
 import Footer from 'src/components/Footer';
 import Header from '../Header ';
-
 import FormAddCloth from '../AddCloth/FormAddCloth';
-import Cloth from '../Cloth';
-import ClothList from '../Cloth/ClothList';
 import Main from '../Main';
 import Changemdp from '../Profil/ChangeMdp';
 import Profil from '../Profil';
 import Signin from '../Sign/Signin';
 import Signup from '../Sign/Signup';
 import Outfit from '../Outfit';
-import OutfitCard from '../Outfit/OutfitCard';
-import OutfitList from '../Outfit/OutfitList';
-import Wardrobe from '../Wardrobe';
+
+
 import AddCloth from '../AddCloth';
 // gerer le dé (png pour le moment)
 import Random from '../Random';
@@ -30,62 +27,75 @@ import Random from '../Random';
 
 
 // == Composant
-const App = () => (
-  <div id="app">
+class App extends Component {
+  componentDidMount() {
+    const { fetchClothContent } = this.props;
+    fetchClothContent();
+  }
 
-    <nav>
-      <Header />
-    </nav>
-    <main>
-      <Switch>
-        <Route exact path="/" component={Accueil} />
-        <Route
-          path="/signin"
-          component={Signin}
-        />
-        <Route
-          path="/signup"
-          component={Signup}
-        />
-        <Route
-          path="/wardrobe"
-          component={Wardrobe}
-        />
-        <Route
-          path="/profil"
-          component={Profil}
-        />
-        <Route
-          path="/random"
-          component={Random}
-        />
-        <Route
-          path="/add-new-cloth"
-          component={AddCloth}
-        />
-        <Route
-          path="/form-new-cloth"
-          component={FormAddCloth}
-        />
-        <Route
-          path="/outfit-id"
-          component={Outfit}
-        />
-        <Route
-          path="/user-page"
-          component={Main}
-        />
-        <Route
-          path="/change-mdp"
-          component={Changemdp}
-        />
-      </Switch>
-    </main>
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-);
+  render() {
+    return (
+      <div id="app">
+
+        <nav>
+          <Header />
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Accueil} />
+            <Route
+              path="/signin"
+              component={Signin}
+            />
+            <Route
+              path="/signup"
+              component={Signup}
+            />
+            <Route
+              path="/wardrobe"
+              component={Wardrobe}
+            />
+            <Route
+              path="/profil"
+              component={Profil}
+            />
+            <Route
+              path="/random"
+              component={Random}
+            />
+            <Route
+              path="/add-new-cloth"
+              component={AddCloth}
+            />
+            <Route
+              path="/form-new-cloth"
+              component={FormAddCloth}
+            />
+            <Route
+              path="/outfit-id"
+              component={Outfit}
+            />
+            <Route
+              path="/user-page"
+              component={Main}
+            />
+            <Route
+              path="/change-mdp"
+              component={Changemdp}
+            />
+          </Switch>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  fetchClothContent: PropTypes.func.isRequired,
+};
 
 // == Export
 export default App;
