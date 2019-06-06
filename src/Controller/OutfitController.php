5321c4328cdbac6f5ddeb1aa208a5f055772c588
KevinDumetz <k.dumetz@live.fr>
2019-06-06 16:41:02 +0200

    controller CRUD API ok + architecture back office

--- src/Controller/OutfitController.php
+++ src/Controller/OutfitController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 
-use App\Repository\OutfitRepository;
-use App\Repository\UserRepository;
+use App\Entity\Outfit;
+use App\Form\OutfitType;
 
+use App\Repository\UserRepository;
+use App\Repository\OutfitRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -52,6 +56,103 @@ class OutfitController extends AbstractController
         // outfit_cloths retourne = un  Outfit :id, name, createdAt + les Cloths associés :  id, name, image, createdAt + le Style : id, name + Type : id, name
 
         return JsonResponse::fromJsonString($json);
+    }
+
+    /**
+     * @Route("/outfit/new", name="new_outfit", methods={"GET", "POST"})
+     */
+    public function new(Request $request) {
+
+        $newOutfit = new Outfit();
+
+        // Using a symfony form
+        $form = $this->createForm(OutfitType::class, $newOutfit);
+
+        // Retrieving data send from REACT ( don't know if we really need to decode )
+        $data = json_decode($request->getContent(), true);
+
+        // Giving the data to the form, a submission
+        $form->submit($data);
+
+        // Do we need to handle the request ?
+        // $form->handleRequest($request);
+
+        // Testing
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On récupère l'utilisateur connecté via le token - TODO
+            // $user = '';
+            // On set l'utilisateur a l'outfit pour lié les deux - TODO
+            // $newOutfit->setUser($user);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newOutfit);
+            $em->flush();
+
+            // Return a json response that show to the front that the creation is successfull ( flash message )
+            // Doit on retourner d'autres infos ? comme l'id de la tenue ?
+            return new JsonResponse(array('flash' => 'La tenue a été ajoutée avec succès !'));
+        }
+
+        // Else
+        else {
+
+            // Return a json response that show to the front that the creation was not successfull ( flash message )
+            return new JsonResponse(array('flash' => 'La tenue n\'a pas pu être ajoutée !'));
+        }
+
+    }
+
+    /**
+     * @Route("/outfit/{id}/edit", name="edit_outfit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Outfit $outfit): Response {
+
+        // Using a symfony form
+        $form = $this->createForm(OutfitType::class, $outfit);
+
+        // Retrieving data send from REACT ( don't know if we really need to decode )
+        $data = json_decode($request->getContent(), true);
+
+        // Giving the data to the form, a submission
+        $form->submit($data);
+
+        // Do we need to handle the request ?
+        // $form->handleRequest($request);
+
+        // Testing
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($outfit);
+            $em->flush();
+
+            // Return a json response that show to the front that the creation is successfull ( flash message )
+            // idem pour le retour create
+            return new JsonResponse(array('flash' => 'La tenue a été modifiée avec succès !'));
+        }
+
+        // Else
+        else {
+
+            // Return a json response that show to the front that the creation was not successfull ( flash message )
+            return new JsonResponse(array('flash' => 'La tenue n\'a pas pu être modifiée !'));
+        }
+
+    }
+
+    /**
+     * @Route("/outfit/{id}/delete", name="delete_outfit", methods={"GET"})
+     */
+    public function delete(Request $request, Outfit $outfit): Response {
+    
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($outfit);
+        $entityManager->flush();
+
+        // Return a json response that show to the front that the delete is successfull OR NOT ( flash message )
+        return new JsonResponse(array('flash' => 'La tenue a été supprimée !'));
+
     }
 
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,25 +2,29 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
+use App\Entity\Role;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/api", name="api_")
  */
-class UserController extends AbstractController
-{
+class UserController extends AbstractController {
     /**
      * Retourne les informations de l'utilisateur(id,username,email,createdAt,role)
      * 
      * @Route("/user/{id}", name="user_show", methods={"GET"})
      */
-    public function show(UserRepository $repository, $id, SerializerInterface $serializer)
-    {
+    public function show(UserRepository $repository, $id, SerializerInterface $serializer) {
 
         $user = $repository->findById($id);
 
@@ -31,6 +35,50 @@ class UserController extends AbstractController
         // user_show retourne = un User : id ,username, email, createdAt et son role: name
 
         return JsonResponse::fromJsonString($json);
+    }
+
+    /**
+     * @Route("/register", name="register", methods={"GET","POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response {
+
+        $user = new User();
+
+        // TODO
+
+        $form = $this->createForm(SubscribeType::class, $user);
+
+        // Retrieving data send from REACT ( don't know if we really need to decode )
+        $data = json_decode($request->getContent(), true);
+
+        // Giving the data to the form, a submission
+        $form->submit($data);
+
+        // $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $hash = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $roleRepository = $this->getDoctrine()->getRepository(Role::class);
+            $role = $roleRepository->findOneBy(['name' => 'Utilisateur']);
+            $user->setRole($role);
+            
+            $manager->persist($user);
+            $manager->flush();
+            
+            // Return a json response that show to the front that the creation is successfull ( flash message )
+            return new JsonResponse(array('flash' => 'Vous vous êtes inscrit avec succès !'));
+
+        }
+        if ($form->isSubmitted() && !$form->isValid()) {
+        
+            // Return a json response that show to the front that the creation was not successfull ( flash message )
+            return new JsonResponse(array('flash' => 'L\'inscription n\'a pas pu être effectuée !'));
+            
+        }
+
     }
 
 }
