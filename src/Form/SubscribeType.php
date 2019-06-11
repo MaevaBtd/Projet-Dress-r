7a19c7b47fab5c25184fb28012b9dc9123f5678c
KevinDumetz <46320672+KevinDumetz@users.noreply.github.com>
2019-06-11 11:49:06 +0200

    Update SubscribeType.php

--- src/Form/SubscribeType.php
+++ src/Form/SubscribeType.php
@@ -14,7 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 
-class UserType extends AbstractType
+class SubscribeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
