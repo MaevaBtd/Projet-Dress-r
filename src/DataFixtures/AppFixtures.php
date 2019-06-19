5c5459fa94eddd4c0d6f845cb36f23c01c048695
Anneb1309 <anneb_38@hotmail.fr>
2019-06-19 09:52:43 +0200

    Fixtures change ( only Type Style User ans Role)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,6 +17,7 @@ use App\Entity\Outfit;
 
 class AppFixtures extends Fixture
 {
+  
     private $encoder;
 
     public function __construct(UserPasswordEncoderInterface $encoder)
@@ -26,12 +27,40 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        $styles = [
+
+            'soirée',
+            'hypster',
+            'hiver',
+            'cozy',
+            'automne',
+            'printemps',
+            'été',
+            'plage',
+            'maison',
+            'sortie',
+            'tendance',
+            'bohème',
+            'chic',
+            'sport',
+            'flemme',
+            'classique',
+            'casual',
+            'classe',
+            'montagne',
+    
+        ];
+
+        $types = [
+            'tête',
+            'haut',
+            'veste',
+            'bas',
+            'chaussures'
+        ];
         
-        $generator = Factory::create('fr_FR');
-        $populator = new Populator($generator, $manager);
-
-        $generator->addProvider(new DressrProvider($generator));
-
+        
+        
         $roleUser = new Role();
         $roleUser->setCode('ROLE_USER');
         $roleUser->setName('Utilisateur');
@@ -56,69 +85,27 @@ class AppFixtures extends Fixture
         $user->setRole($roleUser);
         $user->setNbRandom(0);
 
-       
+        
+        foreach($styles as $style){
+        
+            $newStyle = new Style();
+            $newStyle->setName($style);
+            $manager->persist($newStyle);
+
+        }
+        foreach($types as $type){
+        
+            $newType = new Type();
+            $newType->setName($type);
+            $manager->persist($newType);
+                
+        }
         $manager->persist($roleAdmin);
         $manager->persist($roleUser);
         
         $manager->persist($user);
         $manager->persist($admin);
 
-    
-
-        $populator->addEntity(Type::class, 5, array(
-            'name' => function () use ($generator) {
-                return $generator->types();
-            }
-        ));
-
-        $populator->addEntity(Style::class, 10, array(
-            'name' => function () use ($generator) {
-                return $generator->styles();
-            }
-        ));
-        
-        $populator->addEntity(Cloth::class, 20, array(
-            'name' => function () use ($generator) {
-                return $generator->cloths();
-            },
-            'image' => function () use ($generator) {
-                return $generator->imageCloths();
-            }
-        ));
-
-        $populator->addEntity(Outfit::class, 4, array(
-            'name' => function () use ($generator) {
-                return $generator->outfits();
-            }
-        ));
-
-        $insertedEntities = $populator->execute();
-        
-        $cloths = $insertedEntities['App\Entity\Cloth'];
-        $styles = $insertedEntities['App\Entity\Style'];
-        $outfits = $insertedEntities['App\Entity\Outfit'];
-        
-
-        foreach ($cloths as $cloth) {
-            shuffle($styles);
-            $cloth->addStyle($styles[0]);
-            $cloth->addStyle($styles[1]);
-            $cloth->setUser($user);
-
-            $manager->persist($cloth);
-        }
-
-        foreach ($outfits as $outfit) {
-            shuffle($cloths);
-            $outfit->addCloth($cloths[0]);
-            $outfit->addCloth($cloths[1]);
-            $outfit->addCloth($cloths[2]);
-            $outfit->addCloth($cloths[3]);
-            $outfit->addCloth($cloths[4]);
-            $outfit->setUser($user);
-
-            $manager->persist($outfit);
-        }
         $manager->flush();
 
     }
