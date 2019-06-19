fd414504ad5fe856dba058179716590d9436e10b
MaevaBtd <melizenn@gmail.com>
2019-06-19 12:10:07 +0200

    modals confirm delete outfit and cloth ok

--- src/components/Cloth/index.js
+++ src/components/Cloth/index.js
@@ -1,6 +1,10 @@
 // == Import: Yarn
 import React from 'react';
-import { Icon } from 'antd';
+import {
+  Icon,
+  Modal,
+  Button,
+} from 'antd';
 import 'antd/dist/antd.css';
 import PropTypes from 'prop-types';
 
@@ -11,18 +15,34 @@ import './Cloth.scss';
 // == Code
 // eslint-disable-next-line react/prefer-stateless-function
 class Cloth extends React.Component {
+  handleShowRemoveCloth = () => {
+    const { onShowModal } = this.props;
+    onShowModal();
+  }
+
+  handleCancel = () => {
+    const { onShowModal } = this.props;
+    onShowModal();
+  }
+
+  handleRemoveCloth = () => {
+    const { onShowModal, onRemoveCloth } = this.props;
+    onShowModal();
+    onRemoveCloth();
+  }
+
+
   render() {
     const {
       name,
       image,
       styles,
       type,
-      onRemoveCloth,
+      showModalDelete,
     } = this.props;
-    // const imageCloth = image.split('/');
 
     return (
-      <a id="clothcard">
+      <div id="clothcard">
         <div id="clothcard-content">
           <p>Nom du vêtement: <span className="dyna">{name}</span></p>
           <p>Catégorie:{styles.map(style => (
@@ -32,9 +52,35 @@ class Cloth extends React.Component {
           <p>Type:<span className="dyna">{type.name}</span></p>
         </div>
         <img src={`../../../public/uploads/images/${image}`} alt="" />
-        <Icon id="close-button" type="close-circle" theme="filled" onClick={onRemoveCloth} />
-      </a>
-    )
+        <Icon id="close-button" type="close-circle" theme="filled" onClick={this.handleShowRemoveCloth} />
+        <Modal
+          id="random-modal"
+          visible={showModalDelete}
+          title="Confirmation de suppression"
+          onOk={this.handleRemoveCloth}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button
+              key="back"
+              onClick={this.handleCancel}
+            >
+              Annuler
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              onClick={this.handleRemoveCloth}
+            >
+              Supprimer
+            </Button>,
+          ]}
+        >
+          <h1>{name}</h1>
+          <div>Etes vous sur de vouloir supprimer ce vêtement? </div>
+          <h2>Attention ce vêtement sera aussi supprimé des tenues auquelles vous l'avez attribué</h2>
+        </Modal>
+      </div>
+    );
   }
 }
 
@@ -45,6 +91,8 @@ Cloth.propTypes = {
   styles: PropTypes.array.isRequired,
   type: PropTypes.object.isRequired,
   onRemoveCloth: PropTypes.func.isRequired,
+  showModalDelete: PropTypes.bool.isRequired,
+  onShowModal: PropTypes.func.isRequired,
 };
 
 // == Export
--- src/components/Outfit/OutfitCard.js
+++ src/components/Outfit/OutfitCard.js
@@ -1,6 +1,6 @@
 // == Import: Yarn
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Modal, Button } from 'antd';
 import 'antd/dist/antd.css';
 import PropTypes from 'prop-types';
 
@@ -8,25 +8,76 @@ import PropTypes from 'prop-types';
 import './OutfitCard.scss';
 
 // == Code
-const OutfitCard = ({ name, cloths, onRemoveOutfit }) => (
-  <div id="outfitcard">
-    <div>
-      <p> Nom de la tenue: <span className="dyna"> {name}</span></p>
-      <p>Liste des vêtements:</p>
-      <ul className="dyna">
-        {cloths.map(cloth => (
-          <li
-            key={cloth.id}
-            {...cloth}
-          >
-            {cloth.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-    <Icon id="close-button" type="close-circle" theme="filled" onClick={onRemoveOutfit} />
-  </div>
-);
+class OutfitCard extends React.Component {
+  handleShowRemoveOutfit = () => {
+    console.log('showdeletemodal');
+    const { onShowModal } = this.props;
+    onShowModal();
+  }
+
+  handleCancel = () => {
+    const { onShowModal } = this.props;
+    onShowModal();
+  }
+
+  handleRemoveOutfit = () => {
+    const { onShowModal, onRemoveOutfit } = this.props;
+    onShowModal();
+    onRemoveOutfit();
+  }
+  
+  render() {
+    const { name, cloths, showModalOutfit } = this.props;
+    return (
+      <div id="outfitcard">
+        <div>
+          <p> Nom de la tenue: <span className="dyna"> {name}</span></p>
+          <p>Liste des vêtements:</p>
+          <ul className="dyna">
+            {cloths.map(cloth => (
+              <li
+                key={cloth.id}
+                {...cloth}
+              >
+                {cloth.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Icon id="close-button" type="close-circle" theme="filled" onClick={this.handleShowRemoveOutfit} />
+        <Modal
+          id="random-modal"
+          visible={showModalOutfit}
+          title="Confirmation de suppression"
+          onOk={this.handleRemoveOutfit}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button
+              key="back"
+              onClick={this.handleCancel}
+            >
+              Annuler
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              onClick={this.handleRemoveOutfit}
+            >
+              Supprimer
+            </Button>,
+          ]}
+        >
+          <h1>{name}</h1>
+          <div>Etes vous sur de vouloir supprimer cette tenue ? </div>
+          <h2>les vêtements que vous avez attribué à cette tenue ne serons pas supprimé.
+          Vous pourrez toujours les retrouver dans votre garde-robe et les attribuer à une nouvelle tenue
+          </h2>
+        </Modal>
+      </div>
+    );
+  }
+}
+
 
 OutfitCard.propTypes = {
   // id: PropTypes.number.isRequired,
